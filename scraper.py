90aea291f7c58a3a7199dac3364afe00379d67b1
Martin Kozlovsky <kozlovsky.m7@gmail.com>
2022-07-22 14:36:09 +0200

    added scraper

---
+++ scraper.py
@@ -0,0 +1,57 @@
+from typing import Final
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import chromedriver_autoinstaller
+
+import sys
+
+
+URL: Final[str] = 'https://www.sreality.cz/en/search/for-sale/apartments'
+
+
+def load_page() -> BeautifulSoup:
+    chromedriver_autoinstaller.install()
+
+    options: Options = Options()
+    options.add_argument("--headless")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
+    driver = webdriver.Chrome(options=options)
+    driver.get(URL)
+    driver.delete_cookie('per_page')
+    driver.add_cookie({
+        'name': 'per_page',
+        'value': '500',
+        'domain': 'www.sreality.cz'
+    })
+    driver.refresh()
+
+    try:
+        WebDriverWait(driver, 30).until(
+            EC.presence_of_element_located((By.CLASS_NAME, "name"))
+        )
+    except TimeoutError:
+        print("Timeout when waiting for the list of apartments.",
+            file=sys.stderr)
+
+    source: str = driver.page_source
+
+    driver.quit()
+
+    return BeautifulSoup(source, 'html.parser')
+
+
+def scrape() -> list[dict[str, str]]:
+    soup: BeautifulSoup = load_page()
+    props: list[dict[str, str]] = []
+    for prop in soup.find_all(class_='property'):
+        props.append({
+            'name': prop.find(class_='name').text,
+            'locality': prop.find(class_='locality').text,
+            'img': prop.find('img').attrs['src'],
+        })
+    return props
