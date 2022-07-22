cd3c840834d19c8b26bcc25a2106fbc46ec8899f
Martin Kozlovsky <kozlovsky.m7@gmail.com>
2022-07-22 16:57:37 +0200

    html created from data in database

---
+++ html_gen.py
@@ -0,0 +1,48 @@
+import psycopg
+from psycopg.rows import dict_row
+
+from database import read_config
+
+
+Property = dict[str, str]
+
+
+def prop_to_html(prop: Property) -> str:
+    return f'''
+      <div>
+        <h2>
+          <a href="{prop['url']}">
+            {prop['name']}
+          </a>
+        </h2>
+        <h3>{prop['locality']}</h3>
+        <img src="{prop['img']}" alt="{prop['locality']}">
+      </div>
+    '''
+
+
+def generate_page() -> None:
+    with psycopg.connect(**read_config()) as conn:
+        with conn.cursor(row_factory=dict_row) as cur:
+            cur.execute('SELECT name, locality, img, url FROM properties')
+
+            props_html = ''.join(prop_to_html(prop) for prop in cur)
+            html = f'''
+              <!DOCTYPE html>
+              <html lang="cs-cz">
+                <head>
+                  <meta charset="utf-8">
+                  <title>apartments for sale</title>
+                  <link rel="icon" href="data:,">
+                  <style>
+                    div {{border-bottom: 4px solid black}}
+                  </style>
+                </head>
+                <body>
+                  {props_html}
+                </body>
+              </html>
+            '''
+
+    with open('index.html', 'w') as file:
+        file.write(html)
--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from typing import Final
 from database import save_to_db
 from scraper import scrape
 from server import start_server
-from utils import generate_page
+from html_gen import generate_page
 
 
 HOSTNAME: Final[str] = '0.0.0.0'
@@ -13,5 +13,5 @@ PORT: Final[int] = 8080
 if __name__ == '__main__':
     props = scrape()
     save_to_db(props)
-    generate_page(props)
+    generate_page()
     start_server(HOSTNAME, PORT)
